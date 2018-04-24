7aa55506de09ac767fb4b1cd36e19ee7beac47ee
Shresth Verma <vermashresth@gmail.com>
2018-04-25 01:42:02 +0530

    intro

--- ggplotintro.R
+++ ggplotintro.R
@@ -13,3 +13,19 @@ ggplot(data = mpg, aes(y = displ, x = hwy)) +
 
 ggplot(data = mpg, aes(y = displ, x = hwy)) + 
   geom_point(mapping = aes(colour = displ < 5))
+
+ggplot(data = mpg, aes(x = displ, y = hwy)) +
+  geom_point() +
+  facet_wrap(~ manufacturer)
+
+ggplot(data = mpg, aes(x = displ, y = hwy, color = class)) +
+  geom_point() +
+  facet_wrap(~ manufacturer) +
+  theme_grey()
+
+ggplot(mpg, aes(x = drv, y = hwy)) + 
+  geom_violin()
+
+ggplot(data = mpg, aes(x = fl)) + 
+  geom_bar() +
+  stat_density()
